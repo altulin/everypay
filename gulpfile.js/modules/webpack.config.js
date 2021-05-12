387a9f57d0e0b61dc70a920bdcc9e7ce01a6f1b8
Alexander <al.tulin@gmail.com>
2021-05-12 17:14:25 +0800

    g

--- gulpfile.js/modules/webpack.config.js
+++ gulpfile.js/modules/webpack.config.js
@@ -2,21 +2,8 @@ module.exports = {
   mode: 'none',
   // entry: `/_src/js/main.js`,
   optimization: {
-    // minimize: false,
+    minimize: false,
 
   },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            ['latest', { modules: false }],
-          ],
-        },
-      },
-    ],
-  },
+
 }
